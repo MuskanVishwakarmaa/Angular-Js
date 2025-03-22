818f546996bcc366e1c37d04ba413859127356db
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-22 14:49:24 +0530

    Launching BridgeLabz Site in a new Tab on clicking BridgeLabz Logo

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Helloworld";
   imgUrl = "assets/bridgelabzLogo.webp";
+  url = "https://www.bridgelabz.com/";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz!!!";
+  }
+
+  onClick($event: Event) {
+    console.log("Save button is Clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
